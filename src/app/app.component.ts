bf39d056d80b31a948727eeeb7f4ea943130eae8
Alexis Hernandez <jalapeno9597@gmail.com>
2018-04-24 14:23:37 -0400

    Adding more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,10 +33,9 @@ export class MyApp {
       { title: 'Home', component: HomePage, active: true, icon: 'home' },
       { title: 'Canvas', component: HomePage, active: false, icon: 'custom-Canvas' },
       { title: 'Map', component: MapPage, active: false, icon: 'custom-map' },
-      { title: 'More...', component: MorePage, active: false, icon: 'ionic' },
       { title: 'TapInGo', component: TapComponent, active: false, icon: 'custom-tap' },
-      { title: 'Settings', component: SettingsPage, active: false, icon: 'ionic' },
-      { title: 'AR', component: ArUiPage, active: false, icon: 'ionic' }
+      { title: 'Settings', component: SettingsPage, active: false, icon: 'custom-settings' },
+      { title: 'AR', component: ArUiPage, active: false, icon: 'camera' }
 
     ];
 
